6ea5017424a3c12bca77867379d0df2881eda29b
Khusnutdinov Radmir <XTreme95@yandex.ru>
2016-08-18 11:57:03 +0300

    Adding new entity Employee

---
+++ TransportNetwork.Test/CRUDOperationsWithEnties.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TransportNetwork.Test
+{
+    [TestFixture]
+    public class CRUDOperationsWithEnties
+    {
+        [Test]
+        public void AddingNewEmployeeInCarrierCompany()
+        {
+
+            Employee newEmployee = new Employee();
+            Assert.That(newEmployee, Is.Not.Null);
+
+        }
+
+
+
+    }
+}
