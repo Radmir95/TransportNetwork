c66b740e88872000a325ce3751aa09b1f0771d6e
Khusnutdinov Radmir <XTreme95@yandex.ru>
2016-08-24 11:36:23 +0300

    Create factory for bus

---
+++ TransportNetwork.Domain/Factory/BusFactory.cs
@@ -0,0 +1,16 @@
+﻿using TransportNetwork.Domain.Entity;
+
+namespace TransportNetwork.Domain.Factory
+{
+    public class BusFactory
+    {
+
+        public Bus Create(string numberPlate, string brand, string model, int numberOfSeats)
+        {
+            Bus bus = new Bus(brand, model, numberOfSeats);
+            bus.SetNumberPlate(numberPlate);
+            return bus;
+        }
+
+    }
+}
