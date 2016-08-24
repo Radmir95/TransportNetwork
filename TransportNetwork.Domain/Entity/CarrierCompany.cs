d7bf2e29a85ce6bc072c52cff3dad48c04c24b45
Khusnutdinov Radmir <XTreme95@yandex.ru>
2016-08-24 11:58:47 +0300

    Carrier company entity was improved

--- TransportNetwork.Domain/Entity/CarrierCompany.cs
+++ TransportNetwork.Domain/Entity/CarrierCompany.cs
@@ -4,14 +4,37 @@ namespace TransportNetwork.Domain.Entity
 {
     public class CarrierCompany
     {
+
+        public List<Employee> Employees { get; }
+        public List<Bus> Busses { get; }
+        public int CarrierCompanyId { get; private set; }
+        public string Name { get; private set; }
+        public string City { get; private set; }
+        public string Street { get; private set; }
+        public int House { get; private set; }
+
         public CarrierCompany()
         {
+
             Employees = Employees ?? new List<Employee>();
             Busses = Busses ?? new List<Bus>();
         }
 
-        private List<Employee> Employees { get; }
-        private List<Bus> Busses { get; }
+        public CarrierCompany(string name, string city, string street, int house)
+        {
+            Name = name;
+            City = city;
+            Street = street;
+            House = house;
+            Employees = Employees ?? new List<Employee>();
+            Busses = Busses ?? new List<Bus>();
+
+        }
+
+        internal void SetCarrierCompanyId(int carrierCompanyId)
+        {
+            CarrierCompanyId = carrierCompanyId;
+        }
 
         public void AddNewEmployee(Employee newEmployee)
         {
