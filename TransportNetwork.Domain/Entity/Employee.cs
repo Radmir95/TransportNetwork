edd0080345809c5579638cd0ef4fd66c8f3ed61d
Khusnutdinov Radmir <XTreme95@yandex.ru>
2016-08-24 11:12:24 +0300

    Adding constructor in Employee

--- TransportNetwork.Domain/Entity/Employee.cs
+++ TransportNetwork.Domain/Entity/Employee.cs
@@ -2,14 +2,41 @@
 {
     public class Employee
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PaternalName { get; set; }
-        public int Experience { get; set; }
-        public string City { get; set; }
-        public string Street { get; set; }
-        public int House { get; set; }
-        public int Room { get; set; }
-        public string Telephone { get; set; }
+        public int EmployeeId { get; private set; }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public string MiddleName { get; private set; }
+        public int Experience { get; private set; }
+        public string City { get; private set; }
+        public string Street { get; private set; }
+        public int House { get; private set; }
+        public int Room { get; private set; }
+        public string Telephone { get; private set; }
+
+        public Employee(string firstName, string lastName, string middleName, int experience, string city, string street,
+            int house, int room, string telephone)
+        {
+
+            FirstName = firstName;
+            LastName = lastName;
+            MiddleName = middleName;
+            Experience = experience;
+            City = city;
+            Street = street;
+            House = house;
+            Room = room;
+            Telephone = telephone;
+
+        }
+
+        public Employee()
+        {
+        }
+
+        internal void SetEmployeeId(int employeeId)
+        {
+            EmployeeId = employeeId;
+        }
+       
     }
 }
