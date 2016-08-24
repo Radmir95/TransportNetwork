224b8b324216292d814e3ccb3719bb9131182cd8
Khusnutdinov Radmir <XTreme95@yandex.ru>
2016-08-24 11:33:17 +0300

    Adding employee factory

---
+++ TransportNetwork.Domain/Factory/EmployeeFactory.cs
@@ -0,0 +1,18 @@
+﻿using TransportNetwork.Domain.Entity;
+
+namespace TransportNetwork.Domain.Factory
+{
+    public class EmployeeFactory
+    {
+
+        public Employee Create(int employeeId, string firstName, string lastName, string middleName, int experience,
+            string city, string street, int house, int room, string telephone)
+        {
+            Employee employee = new Employee(firstName, lastName, middleName, experience, city, street,
+                house, room, telephone);
+            employee.SetEmployeeId(employeeId);
+            return employee;
+        }
+
+    }
+}
