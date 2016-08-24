f307d99729f7ea023432162cb1742aa805da2a58
Khusnutdinov Radmir <XTreme95@yandex.ru>
2016-08-24 11:44:25 +0300

    Adding exceptions in employee entity

--- TransportNetwork.Domain/Factory/EmployeeFactory.cs
+++ TransportNetwork.Domain/Factory/EmployeeFactory.cs
@@ -5,10 +5,10 @@ namespace TransportNetwork.Domain.Factory
     public class EmployeeFactory
     {
 
-        public Employee Create(int employeeId, string firstName, string lastName, string middleName, int experience,
+        public Employee Create(int employeeId, string firstName, string surName, string middleName, int experience,
             string city, string street, int house, int room, string telephone)
         {
-            Employee employee = new Employee(firstName, lastName, middleName, experience, city, street,
+            Employee employee = new Employee(firstName, surName, middleName, experience, city, street,
                 house, room, telephone);
             employee.SetEmployeeId(employeeId);
             return employee;
