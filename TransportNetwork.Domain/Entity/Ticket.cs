cac6ae344f1c2a3cb7281e5a4e4c8c43d06d3083
Khusnutdinov Radmir <XTreme95@yandex.ru>
2016-08-24 12:26:45 +0300

    Adding constructor in Ticket

--- TransportNetwork.Domain/Entity/Ticket.cs
+++ TransportNetwork.Domain/Entity/Ticket.cs
@@ -5,9 +5,31 @@ namespace TransportNetwork.Domain.Entity
     public class Ticket
     {
         public int TicketId { get; set; }
-        public DateTime? DateOfSale { get; set; }
-        public double Cost { get; set; }
-        public DateTime? DateOfDeparture { get; set; }
-        public int EmployeeId { get; set; }
+        public DateTime? DateOfSale { get; private set; }
+        public double Cost { get; private set; }
+        public DateTime? DateOfDeparture { get; private set; }
+        public Employee Employee { get; private set; }
+
+        public Ticket(DateTime? dateOfSale, double cost, DateTime? dateOfDeparture, Employee employee)
+        {
+            
+            if (DateOfSale == null)
+                throw new ArgumentNullException("DateOfSale can't be null");
+            DateOfSale = dateOfSale;
+            if (DateOfDeparture == null)
+                throw new ArgumentNullException("dateOfDeparture can't be null");
+            DateOfDeparture = dateOfDeparture;
+            if (employee == null)
+                throw new ArgumentNullException("Employee can't be null");
+            Employee = employee;
+            Cost = cost;
+
+        }
+
+        internal void SetTicketId(int ticketId)
+        {
+            TicketId = ticketId;
+        }
+
     }
 }
