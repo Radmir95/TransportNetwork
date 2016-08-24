99622a2c402a26be8262a0e896d85e6914f6842d
Khusnutdinov Radmir <XTreme95@yandex.ru>
2016-08-24 12:51:27 +0300

    Improved class tour

--- TransportNetwork.Domain/Entity/Tour.cs
+++ TransportNetwork.Domain/Entity/Tour.cs
@@ -4,10 +4,32 @@ namespace TransportNetwork.Domain.Entity
 {
     public class Tour
     {
-        public int TourId { get; set; }
-        public DateTime? TimeOfDeparture { get; set; }
-        public DateTime? TimeOfArrival { get; set; }
-        public int Distance { get; set; }
-        public string PointOfDeparture { get; set; }
+        public int TourId { get; private set; }
+        public DateTime? TimeOfDeparture { get; private set; }
+        public DateTime? TimeOfArrival { get; private set; }
+        public int Distance { get; private set; }
+        public string PointOfDeparture { get; private set; }
+
+        public Tour(DateTime? timeOfDeparture, DateTime? timeOfArrival, int distance, string pointOfDeparture)
+        {
+            
+            if (timeOfDeparture == null)
+                throw new ArgumentNullException("TimeOfDeparture can't be null");
+            TimeOfDeparture = timeOfDeparture;
+            if (timeOfArrival == null)
+                throw new ArgumentNullException("Time of arrival can't be null");
+            TimeOfArrival = timeOfArrival;
+            Distance = distance;
+            if (PointOfDeparture == null)
+                throw new ArgumentNullException("PointOfDeparture can't be null");
+            PointOfDeparture = pointOfDeparture;
+
+        }
+
+        internal void SetTourId(int tourId)
+        {
+            TourId = tourId;
+        }
+       
     }
 }
