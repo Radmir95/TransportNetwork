1c9b3fbe9d37f7c2afcb45aca5f168665aa90816
Husnutdinov Radmir <XTreme95@yandex.ru>
2016-08-28 22:13:29 +0300

    Another updating interfaces

---
+++ TransportNetwork.DataAccessLayer/Repository/TourRepository.cs
@@ -0,0 +1,32 @@
+﻿using TransportNetwork.DataAccessLayer.IRepository;
+using TransportNetwork.Domain.Entity;
+
+namespace TransportNetwork.DataAccessLayer.Repository
+{
+    public class TourRepository : ITourRepository
+    {
+        public void AddTour(Tour tour)
+        {
+            
+
+
+
+        }
+
+        public void DeleteTour(Tour tour)
+        {
+            
+
+
+
+        }
+
+        public void UpdateTour(Tour tour)
+        {
+            
+
+
+
+        }
+    }
+}
