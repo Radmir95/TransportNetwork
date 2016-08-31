24f1935c2c3c431e71339c40e2dc05100494696e
Husnutdinov Radmir <XTreme95@yandex.ru>
2016-08-31 22:51:11 +0300

    Adding waybill addWaybill method

--- TransportNetwork.DataAccessLayer/Repository/TourRepository.cs
+++ TransportNetwork.DataAccessLayer/Repository/TourRepository.cs
@@ -1,4 +1,6 @@
 ﻿using Infrastructure.ILoggerManager;
+using System.Data;
+using System.Data.SqlClient;
 using TransportNetwork.DataAccessLayer.IRepository;
 using TransportNetwork.Domain.Entity;
 
@@ -12,9 +14,51 @@ namespace TransportNetwork.DataAccessLayer.Repository
 
         public void AddTour(Tour tour)
         {
-            
 
+            var context = _context.Create();
+            var conn = (SqlConnection)context;
 
+            var cmdAddTour = new SqlCommand("INSERT INTO Tour" +
+            "(timeOfDeparture, timeOfArrival, distance, pointOfDeparture)"
+            + " VALUES (@timeOfDeparture, @timeOfArrival, @distance, @pointOfDeparture)", conn);
+
+            var param = new SqlParameter();
+
+            param.ParameterName = "@timeOfDeparture";
+            param.Value = tour.TimeOfDeparture;
+            param.SqlDbType = SqlDbType.DateTime;
+            cmdAddTour.Parameters.Add(param);
+
+            param = new SqlParameter();
+            param.ParameterName = "@timeOfArrival";
+            param.Value = tour.TimeOfArrival;
+            param.SqlDbType = SqlDbType.DateTime;
+            cmdAddTour.Parameters.Add(param);
+
+            param = new SqlParameter();
+            param.ParameterName = "@distance";
+            param.Value = tour.Distance;
+            param.SqlDbType = SqlDbType.Int;
+            cmdAddTour.Parameters.Add(param);
+
+            param = new SqlParameter();
+            param.ParameterName = "@pointOfDeparture";
+            param.Value = tour.PointOfDeparture;
+            param.SqlDbType = SqlDbType.Text;
+            cmdAddTour.Parameters.Add(param);
+
+            try
+            {
+                cmdAddTour.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                _log.AddExceptionError(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
 
         }
 
---
+++ TransportNetwork.DataAccessLayer/Repository/WaybillRepository.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.ILoggerManager;
+using TransportNetwork.DataAccessLayer.IRepository;
+using TransportNetwork.Domain.Entity;
+
+namespace TransportNetwork.DataAccessLayer.Repository
+{
+    public class WaybillRepository : IWaybillRepository
+    {
+
+        private readonly ConnectionContext _context;
+        private readonly ILogger _log;
+
+        public void AddWaybill(Waybill waybill)
+        {
+            
+
+
+        }
+    }
+}
